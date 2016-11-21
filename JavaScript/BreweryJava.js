f08161f965554c032f7d212a1bac5332e92b852c
grady.shepherd <grady.shepherd@umconnect.umt.edu>
2016-11-21 15:42:12 -0700

    facebook

--- JavaScript/BreweryJava.js
+++ JavaScript/BreweryJava.js
@@ -26,6 +26,6 @@ lightbox.option({
 
 function openFacebook(){
 	event.preventDefault();
-	window.open("Facebook.com://profile/1469884123304503");
+	window.open("fb://profile/1469884123304503");
 	setTimeout(function () { window.open("https://www.facebook.com/2BassetBrewery/?fref=ts"); }, 500);
 }
