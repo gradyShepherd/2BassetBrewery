aa7f0a78c626eb5bb687f3bf9bd66371915e6e28
grady.shepherd <grady.shepherd@umconnect.umt.edu>
2016-11-20 17:15:18 -0700

    fixed footer for xs screens added function to check if app opens when available for facebook links       -unchecked on mobile as of now, opens webpage fine

--- JavaScript/BreweryJava.js
+++ JavaScript/BreweryJava.js
@@ -23,3 +23,8 @@ lightbox.option({
   'wrapAround': true,
   'disableScrolling':true,
 })
+
+function openFacebook(){
+	setTimeout(function () { window.location = "https://www.facebook.com/2BassetBrewery/?fref=ts"; }, 25);
+	window.location = "fb://2BassetBrewery/1469884123304503";
+}
